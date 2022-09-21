487500f4fcf8c82829725c8d344ed48b5709a253
Roopesh Saravanan <roopeshsaravanan.dev@gmail.com>
2022-09-21 19:29:28 +0530

    implemented form validation

---
+++ demo-app/src/components/auth/FormInput.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react"
+
+export default function FormInput(props) {
+  const [focus, setFocus] = useState(false)
+  const { id, label, errorMessage, textColor, ...inputProps } = props
+
+  function handleBlur(e) {
+    setFocus(true)
+  }
+
+  return (
+    <div className='form-group mb-4'>
+      <label className='block text-gray-700 text-sm font-bold mb-2'>
+        {label}
+      </label>
+      <input
+        className='peer shadow border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline'
+        onBlur={handleBlur}
+        onFocus={() => inputProps.name == "confirmPassword" && setFocus(true)}
+        focus={focus.toString()}
+        {...inputProps}
+      ></input>
+      <span className={`text-sm ${textColor}`}>{errorMessage}</span>
+    </div>
+  )
+}
+
+// hidden peer-invalid:block
--- demo-app/src/components/auth/Signin.js
+++ demo-app/src/components/auth/Signin.js
@@ -1,34 +1,62 @@
-import React, { useRef, useState } from "react"
+import React, { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
 // import { useAuth } from "../../contexts/AuthContext"
 import CenteredContainer from "../CenteredContainer"
+import FormInput from "./FormInput"
 
 export default function Signin() {
-  const [error, setError] = useState("")
+  // const [error, setError] = useState("")
   const [loading, setLoading] = useState(false)
-  const emailRef = useRef()
-  const passwordRef = useRef()
-  // const { signin, currentUser } = useAuth()
+  const [values, setValues] = useState({
+    email: "",
+    password: "",
+  })
+  const emailInput = {
+    id: 1,
+    name: "email",
+    type: "email",
+    placeholder: "Email",
+    errorMessage: "Not a valid email address!",
+    label: "Email",
+    required: true,
+    textColor: "text-red-500",
+  }
+  const passwordInput = {
+    id: 2,
+    name: "password",
+    type: "password",
+    placeholder: "Password",
+    required: true,
+    label: "Password",
+  }
   const navigate = useNavigate()
 
-  async function handleSubmit(e) {
-    e.preventDefault()
+  // async function handleSubmit(e) {
+  //   e.preventDefault()
 
-    try {
-      setError("")
-      setLoading(true)
-      // await signin(emailRef.current.value, passwordRef.current.value)
-      setLoading(false)
-      navigate("/profile")
-    } catch (error) {
-      setError("Failed to signin")
-      setLoading(false)
-    }
+  //   try {
+  //     setError("")
+  //     setLoading(true)
+  //     // await signin(emailRef.current.value, passwordRef.current.value)
+  //     setLoading(false)
+  //     navigate("/profile")
+  //   } catch (error) {
+  //     setError("Failed to signin")
+  //     setLoading(false)
+  //   }
+  // }
+
+  function handleSubmit(e) {
+    e.preventDefault()
+  }
+
+  const onChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value })
   }
 
   return (
     <CenteredContainer>
-      <div className='w-full max-w-xs'>
+      <div className='w-full max-w-sm'>
         {/* {error && console.log(error)}
         {console.log(currentUser)} */}
         <form
@@ -38,30 +66,25 @@ export default function Signin() {
           <div className='mb-6'>
             <h2 className='text-2xl font-bold'>Sign In</h2>
           </div>
-          <div className='mb-4'>
-            <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Email
-            </label>
-            <input
-              className='shadow border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline'
-              type='email'
-              ref={emailRef}
-              required
-              placeholder='Email'
-            ></input>
-          </div>
+          <FormInput
+            key={emailInput.id}
+            {...emailInput}
+            value={values[emailInput.name]}
+            onChange={onChange}
+          />
           <div className='mb-6'>
             <label className='block text-gray-700 text-sm font-bold mb-2'>
-              Password
+              {passwordInput.label}
             </label>
             <input
+              key={passwordInput.id}
+              {...passwordInput}
               className='shadow border rounded w-full py-2 px-3 text-gray-700 mb-3 focus:outline-none focus:shadow-outline'
-              type='password'
-              ref={passwordRef}
-              required
-              placeholder='****************'
+              value={values[passwordInput.name]}
+              onChange={onChange}
             ></input>
           </div>
+
           <div className='flex items-center justify-between mb-6'>
             <button
               className='bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline'
